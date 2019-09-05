a0c7fbff3e737230696302d73209b63c2df179e9
Boris <do@itlater.com>
2019-09-05 21:54:48 +0300

    Remove internal state from QRReader

--- frontend/src/components/QRReader/index.jsx
+++ frontend/src/components/QRReader/index.jsx
@@ -2,15 +2,9 @@ import React, {Component} from 'react'
 import QrReader from 'react-qr-reader'
 
 export class QRReader extends Component {
-    state = {
-        result: 'No result'
-    }
-
     handleScan = data => {
         if (data) {
-            this.setState({
-                result: data
-            })
+            this.props.onData(data)
         }
     }
     handleError = err => {
@@ -26,7 +20,6 @@ export class QRReader extends Component {
                     onScan={this.handleScan}
                     style={{width: '100%'}}
                 />
-                <p>{this.state.result}</p>
             </div>
         )
     }
