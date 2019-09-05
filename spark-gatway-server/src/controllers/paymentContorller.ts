5c0c41bf7c7eaf5defc41e6a24504be886009265
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-09-05 21:20:20 +0300

    fix(backend): integrate spark and charge payment

--- spark-gatway-server/src/controllers/paymentContorller.ts
+++ spark-gatway-server/src/controllers/paymentContorller.ts
@@ -1,8 +1,26 @@
 import jsend from 'jsend';
+import axios from 'axios';
 import {Request, Response} from 'express';
 
-export const rootController = (req: Request, res: Response) => {
-	res.send(jsend.success('this is a demo controller'));
+const charge = require('lightning-charge-client')
+('http://charge.tiger.hackbtc19.offchain.rocks', 'jpZ9Ex9kedpD1Q')
+
+interface IInvoice {
+	id: string;
+	msatoshi: string;
+	description: string;
+	rhash: string;
+	payreq: string;
+	expires_at: number;
+	created_at: number;
+	metadata: any;
+	status: string;
+}
+
+export const rootController = async (req: Request, res: Response) => {
+	// Create invoice
+	const inv: IInvoice = await charge.invoice({msatoshi: 50, metadata: {customer_id: 123, product_id: 456}})
+	res.redirect(`http://charge.tiger.hackbtc19.offchain.rocks/checkout/${inv.id}`)
 };
 
 export const qsRootController = (req: Request, res: Response) => {
