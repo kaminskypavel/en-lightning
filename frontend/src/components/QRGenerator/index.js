9a6f95367d5bbf07de11d5fd826876f179afaa07
Boris <do@itlater.com>
2019-09-05 21:54:34 +0300

    Support size in qrcode

--- frontend/src/components/QRGenerator/index.js
+++ frontend/src/components/QRGenerator/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 var QRCode = require('qrcode.react');
 
-export const QRGenerator = ({link}) =>
-    <QRCode value={link} level={"Q"}/>
+export const QRGenerator = ({link, size=200}) =>
+    <QRCode value={link} size={size} level={"Q"}/>
 
