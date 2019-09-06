c0158efc121abe9e3b9ba022fcb3e237673f3c49
Boris <do@itlater.com>
2019-09-06 11:00:46 +0300

    Fix ar

--- frontend/src/Screens/Vendor/PaymentInfo.js
+++ frontend/src/Screens/Vendor/PaymentInfo.js
@@ -7,7 +7,7 @@ const PaymentInfo = ({ onData}) => (
 
     <p>1. Scan QR to initiate payment</p>
 
-    <QRGenerator size={ 300 } link="https://5d720eb385fefb0008be4458--determined-golick-f17160.netlify.com/"/>
+    <QRGenerator size={ 300 } link="https://bit.ly/2kuYj1T"/>
 
     <p>2. Show payment confirmation to camera to proceed</p>
 
